b1aedb33b0b9a1988945813eba9d6694d2feddff
Andrey Malyar <malyar.andrey@list.ru>
2025-08-02 11:27:59 +0500

    feat: Добавлены моковый данные и обновлены стили BoardingDetails

--- src/pages/reviewBooking/BoardingDetails.tsx
+++ src/pages/reviewBooking/BoardingDetails.tsx
@@ -18,7 +18,7 @@ function BoardingDetails (){
     return (
         <div className="review">
             <h3 className="review__title">Boarding Details</h3>
-            <p className="">22426 - VANDE BHARAT<span>Class 2A & Tatkal Quota</span></p>
+            <p className="review__train-info">22426 - VANDE BHARAT<span className="review__train-class">Class 2A & Tatkal Quota</span></p>
             {<RouteInfo departure={departure} arrival={arrival} duration={"8 hours"}/>}
         </div>
     )
